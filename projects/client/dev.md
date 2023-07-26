1b2d1e2939147f8a72a36a05ce4eaea17abc98c4
guojian <2502585266@qq.com>
2023-07-26 18:01:14 +0800

    client: ca证书

---
+++ projects/client/dev.md
@@ -0,0 +1,6 @@
+# 笔记
+
+## 参考
+
+1. [vite集成https，并安装本地自签名证书](https://zhuanlan.zhihu.com/p/551720193)
+2. [真机调试 安卓+Chrome](https://juejin.cn/post/6963540939944132615)
--- projects/client/vite.config.ts
+++ projects/client/vite.config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import path from 'path'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), basicSsl()],
   server: {
-    https: true,
+    https: {
+      cert: fs.readFileSync(path.join(__dirname, 'keys/cert.crt')),
+      key: fs.readFileSync(path.join(__dirname, 'keys/cert.key'))
+    },
     host: '0.0.0.0',
     port: 7788
     // proxy: {}
